82c391bdb379bd9e2aa059d183e6055d6e24a34d
Johannes Schanda <schanda@itestra.de>
2013-01-14 15:32:18 +0100

    Update license headers for genivi release

--- org.genivi.commonapi.core.tests/src/test-interface-stub.cpp
+++ org.genivi.commonapi.core.tests/src/test-interface-stub.cpp
@@ -1,4 +1,7 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright (C) 2013 BMW Group
+ * Author: Manfred Bathelt (manfred.bathelt@bmw.de)
+ * Author: Juergen Gehring (juergen.gehring@bmw.de)
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 #include <commonapi/tests/TestInterfaceStubDefault.h>
